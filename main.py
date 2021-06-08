e5458cc729bf6bd18546bf4b19e81e1353f5eb62
Baily kaupp <>
2021-06-08 19:13:41 +0000

    commit

--- main.py
+++ main.py
@@ -5,22 +5,16 @@ from discord.ext import commands
 
 client = discord.Client()
 
-client = commands.Bot(command_prefix = os.getenv(prefix), help_command=None) 
+client = commands.Bot(command_prefix = os.getenv("prefix"), help_command=None) 
 
 intents = discord.Intents.default()
 intents.presences = True
 
-bad_bad_names = [
-  'twitter.com/h0nde', 'h0nde', 'honde'
-  ]
-#lower case only, anyone who's username contains a string is this list will be kicked
-
-@client.event(enabled=False)
+@client.event
 async def on_member_join(member):
-  if member.name.lower() in bad_bad_names:
+  if member.name.lower.find('h0nde') >= 1:
     member.kick(reason="take a sip 🥤")
     channel = await client.fetch_channel(os.getenv("log_channel"))
-    channel.send('user kicked ({}), take a sip 🥤'.format(member.id))\
-  else:
-     return()
+    channel.send('user kicked ({}), take a sip 🥤'.format(member.id))
+
 client.run(os.getenv("token")) 
