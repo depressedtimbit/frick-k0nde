47a4ea5810e0e673a26dc6fc78d1ffdb62d77f25
depressedtimbit <84045706+depressedtimbit@users.noreply.github.com>
2021-06-08 19:11:16 -0230

    Update main.py

--- main.py
+++ main.py
@@ -45,27 +45,5 @@ async def on_member_join(member):
       await channel.send(ban_text.format(member.mention, member.id))
       break
 
-
-intents = discord.Intents.default()
-intents.presences = True
-client = commands.Bot(command_prefix ='$%', help_command=None) 
-
-@client.event 
-async def on_ready():
-  print('ready')
-
-bad_bad_names = [
-  'twitter.com/h0nde', 'h0nde'
-  ]
-
-@client.event
-async def on_member_join(member):  
-  for item in bad_bad_names:
-    if member.name.lower.find(item):
-      await member.kick(reason="take a sip 🥤")
-      channel = await client.fetch_channel(os.getenv("log_channel"))
-      await channel.send('user kicked ({}), take a sip 🥤'.format(member.id))
-
-
 # Running Bot from Bot Token
 bot.run(getenv('token'))
