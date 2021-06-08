cb598a0f643e2292438c7fc3712d127f46f6b503
Baily kaupp <>
2021-06-08 19:50:48 +0000

    i fucming forgot the names of the bots

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from discord.ext import commands
 
 client = discord.Client()
 
-client = commands.Bot(command_prefix = os.getenv("prefix"), help_command=None) 
+client = commands.Bot(help_command=None) 
 
 intents = discord.Intents.default()
 intents.presences = True
 
 @client.event
 async def on_member_join(member):
-  if member.name.lower.find('h0nde') >= 1:
+  if member.name.lower.find('twitter.com/h0nde') >= 1:
     member.kick(reason="take a sip 🥤")
     channel = await client.fetch_channel(os.getenv("log_channel"))
     channel.send('user kicked ({}), take a sip 🥤'.format(member.id))
