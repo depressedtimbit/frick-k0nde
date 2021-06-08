3a2f8572ade4152055048c2a9fb48557579c0234
Baily kaupp <>
2021-06-08 21:25:51 +0000

    eeee i cant name these

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ from discord.ext import commands
 
 client = discord.Client()
 
-client = commands.Bot(help_command=None) 
-
 intents = discord.Intents.default()
 intents.presences = True
+client = commands.Bot(command_prefix ='$%', help_command=None) 
+
+@client.event 
+async def on_ready():
+  print('ready')
+
+bad_bad_names = [
+  'twitter.com/h0nde', 'h0nde'
+  ]
 
 @client.event
-async def on_member_join(member):
-  if member.name.lower.find('twitter.com/h0nde') >= 1:
-    member.kick(reason="take a sip 🥤")
-    channel = await client.fetch_channel(os.getenv("log_channel"))
-    channel.send('user kicked ({}), take a sip 🥤'.format(member.id))
+async def on_member_join(member):  
+  for item in bad_bad_names:
+    if member.name.lower.find(item):
+      await member.kick(reason="take a sip 🥤")
+      channel = await client.fetch_channel(os.getenv("log_channel"))
+      await channel.send('user kicked ({}), take a sip 🥤'.format(member.id))
 
 client.run(os.getenv("token")) 
